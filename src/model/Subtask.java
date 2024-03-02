478cf6115ffa5c6e78e3be21a23724f0e1368c19
Vladislav Gadetov <vlad-nijakun@mail.ru>
2024-03-02 13:22:18 +0200

    Исправить ошибки в стиле кода

--- src/model/Subtask.java
+++ src/model/Subtask.java
@@ -4,6 +4,7 @@ import java.util.Objects;
 
 public class Subtask extends Task {
     int idEpic;
+
     public Subtask(String title, String description, Epic epic) {
         super(title, description);
         this.idEpic = epic.getId();
